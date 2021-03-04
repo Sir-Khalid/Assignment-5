1c04f6085eb3ac12ab2bd7957d9ac5ea5088e632
Khaled Al-Mansour <khaled.al-mansour@ocsbstudent.ca>
2021-03-04 01:37:44 +0000

    changed errors

--- script.js
+++ script.js
@@ -7,18 +7,17 @@ document.getElementById('button-one').addEventListener('click', area)
 document.getElementById('button-two').addEventListener('click', circumfrence)
 // This function is created for to calculate area and circumfrence
 function area () {
- diameter = document.getElementById('input').value
- diameter = parseInt(diameter)
- radius = diameter / 2
- results = Math.PI * (radius * radius)
+  diameter = document.getElementById('input').value
+  diameter = parseInt(diameter)
+  radius = diameter / 2
+  results = Math.PI * (radius * radius)
  document.getElementById('results').innerHTML = Math.round(results * 10) / 10
 }
 // A another function created for buttonTwo
 function circumfrence () {
- diameter = document.getElementById('input').value
- diameter = parseInt(diameter)
- radius = diameter / 2
- results = 2 * Math.PI * radius
- document.getElementById('results').innerHTML = Math.round(results * 10) / 10
+  diameter = document.getElementById('input').value
+  diameter = parseInt(diameter)
+  radius = diameter / 2
+  results = 2 * Math.PI * radius
+  document.getElementById('results').innerHTML = Math.round(results * 10) / 10
 }
-
